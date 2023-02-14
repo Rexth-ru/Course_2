3800bdd652cebc1ff2e16542ab022a4c2086a6ea
Rexth-ru <119601642+Rexth-ru@users.noreply.github.com>
2023-02-14 12:42:21 +0300

    Update TaskService.java

--- src/tasks/TaskService.java
+++ src/tasks/TaskService.java
@@ -8,10 +8,11 @@ import java.util.stream.Collectors;
 public class TaskService{
    private Map<Integer, Task> taskMap = new HashMap<>();
    private Collection<Task> removedTasks;
-
+//добавить задачу
    public void add(Task task){
        taskMap.put(task.getId(), task);
    }
+//удалить задачу
    public Task remove(int id) throws TaskNotFoundException{
        if (!taskMap.containsKey(id)){
            throw new TaskNotFoundException();
@@ -19,6 +20,7 @@ public class TaskService{
            return  taskMap.remove(id);
        }
    }
+ //получить задачи на определенную дату
    public Collection<Task> getAllByDate(LocalDate date)  {
 
        return taskMap.values().stream().filter((Task x) -> x.appearsln(date)).collect(Collectors.toList());
